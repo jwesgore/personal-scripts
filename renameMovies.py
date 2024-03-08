f2bfb2629c035ab5f4cf501a1158634030ff52cb
Gore, James <jwesgore@gmail.com>
2024-03-08 17:29:20 -0600

    added support for finding subtitles

--- renameMovies.py
+++ renameMovies.py
@@ -29,13 +29,25 @@ def input_is_directory(input_folder:Path) -> Path:
         search = list(input_folder.glob(f"*{extension}"))
         if len(search) > 0:
             return search[0]
-
+        
+def get_subtitle_track(input_file:Path):
+    '''search for subtitle track'''
+    search = list(input_file.parent.glob("*.srt"))
+    if len(search) > 0:
+        return search[0]
+    else:
+        return False
+    
 def move_movie(arg:Path):
 
+    # search for movie file and subtitles
     if arg.is_dir():
         movie_file = input_is_directory(arg)
     else:
         movie_file = arg
+
+    subtitle_track = get_subtitle_track(movie_file)
+
     os.chdir(movie_file.parent)
 
     # build new movie name
@@ -46,14 +58,21 @@ def move_movie(arg:Path):
 
     # rename file
     movie_file = movie_file.parent / movie_file.rename(movie_rename)
+    
+    if subtitle_track:
+        subtitle_track = subtitle_track.parent / subtitle_track.rename(movie_file.stem)
 
     # convert file
     if movie_file.suffix in convert_if_extension:
         movie_file = convert(movie_file)
    
-    # move file and delete parent dir
+    # move file and subtitle track
     shutil.move(movie_file, movies_folder)
-    
+
+    if subtitle_track:
+        shutil.move(subtitle_track, movies_folder)
+
+    # delete parent dir if input is dir
     if arg.is_dir():
         os.chdir(movie_file.parents[1])
         shutil.rmtree(movie_file.parent)
