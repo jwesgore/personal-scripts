fe7c59a21cbbfb5fc23956907cfa4e108fe545fa
Gore, James <jwesgore@gmail.com>
2024-03-08 19:51:08 -0600

    wrap in try statements

--- renameMovies.py
+++ renameMovies.py
@@ -1,5 +1,7 @@
 '''
 script for batch renaming movie files and moving them to movies folder
+required that yaml and requests libraries are installed, can be installed with pip
+required that ffmpeg is installed for conversion, can be installed with winget
 '''
 
 import os, sys, yaml, re, shutil, subprocess, requests
@@ -12,11 +14,14 @@ convert_to_extension = ".mp4"
 
 def plex_update_libraries():
     '''atttempt to run "Update Libraries" on Plex'''
-    with open("plex_data.yml") as f:
-        plex_data = yaml.load(f, Loader=yaml.FullLoader)
-    url = f"{plex_data['plex_address']}/library/sections/1/refresh?X-Plex-Token={plex_data['plex_token']}"
-    requests.get(url)
-
+    try:
+        with open("plex_data.yml") as f:
+            plex_data = yaml.load(f, Loader=yaml.FullLoader)
+        url = f"{plex_data['plex_address']}/library/sections/1/refresh?X-Plex-Token={plex_data['plex_token']}"
+        requests.get(url)
+    except:
+        print("Error encountered while updating Plex library")
+    
 def check_title(title:str) -> str:
     '''special checks for errors in title'''
     # check if title has ( at end
@@ -26,13 +31,16 @@ def check_title(title:str) -> str:
 
 def convert(movie_path: Path) -> Path:
     '''convert movie to different file format using ffmpeg'''
-    new_file = f"{movie_path.stem}{convert_to_extension}"
-    subprocess.run(f'ffmpeg -i "{movie_path.name}" "{new_file}"', cwd=movie_path.parent)
-    return movie_path.parent / new_file
+    try:
+        new_file = f"{movie_path.stem}{convert_to_extension}"
+        subprocess.run(f'ffmpeg -i "{movie_path.name}" "{new_file}"', cwd=movie_path.parent)
+        return movie_path.parent / new_file
+    except:
+        print("Conversion failed or ffmpeg not installed")
+        return movie_path
 
 def input_is_directory(input_folder:Path) -> Path:
     '''search directory for movie file if movie is dir'''
-
     for extension in extensions:
         search = list(input_folder.glob(f"*{extension}"))
         if len(search) > 0:
@@ -90,7 +98,4 @@ def move_movie(arg:Path):
 if __name__ == '__main__':
     for arg in sys.argv[1:]:
         move_movie(Path(arg))
-    try:
-        plex_update_libraries()
-    except:
-        print("Error encountered while updating Plex library")
+    plex_update_libraries()
